305e119229aa47a78b05f3b76977643dd9b134e5
Mohamed B <79986800+mbouchenguour@users.noreply.github.com>
2024-12-10 09:21:45 +0100

    tag

--- Administration des reseaux/TP/TP3/conf-sw1.sh
+++ Administration des reseaux/TP/TP3/conf-sw1.sh
@@ -1,3 +1,5 @@
 ovs-vsctl set port eth1 tag=10
 ovs-vsctl set port eth2 tag=20
 ovs-vsctl set port eth3 trunks=10,20
+
+ovs-vsctl set port eth4 tag=20
--- Administration des reseaux/TP/TP3/conf-sw2.sh
+++ Administration des reseaux/TP/TP3/conf-sw2.sh
@@ -1,2 +1,3 @@
 ovs-vsctl set port eth0 tag=10
 ovs-vsctl set port eth1 tag=20
+ovs-vsctl set port eth2 tag=10
